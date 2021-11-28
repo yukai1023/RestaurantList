7fb8475b2f8a6fc829a52824cf481d85d86ac22a
Yukai <91178978+yukai1023@users.noreply.github.com>
2021-11-28 22:08:30 +0800

    Update app.js

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.get('/restaurants/:restaurants_id', (req, res) => {
 app.get('/search', (req, res) => {
   const keyword = req.query.keyword
   const restaurant = restaurantList.results.filter(restaurant => {
-    return restaurant.name.toLowerCase().includes(keyword.toLowerCase())
+    return restaurant.name.toLowerCase().includes(keyword.toLowerCase()) || restaurant.category.toLowerCase().includes(keyword.toLowerCase())
   })
   res.render('index', { restaurants: restaurant, keyword: keyword })
 })
